4a6eeaf831b67e0543254cf45423edd70d00ea19
Jeff Anderson <jeffa2@vmware.com>
2023-02-02 11:45:50 -0600

    updates

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("ZZZGreetings from Spring Boot + Tanzu!", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("ZZZGreetings from Spring Boot + Tanzu!"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu!"));
     }
 }
